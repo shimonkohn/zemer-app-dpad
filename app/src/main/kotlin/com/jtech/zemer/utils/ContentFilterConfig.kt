75ba9c6949da35343c8cd46ffd9fecd2d089b340
tripleu613 <tripleuworld@gmail.com>
2025-11-30 23:27:05 -0500

    haaaaaaaaaaaaaaaa

---
+++ app/src/main/kotlin/com/jtech/zemer/utils/ContentFilterConfig.kt
@@ -0,0 +1,22 @@
+package com.jtech.zemer.utils
+
+/**
+ * In-memory representation of content filter settings pulled from DataStore.
+ */
+data class ContentFilterConfig(
+    val filtersEnabled: Boolean = true,
+    val allowFemaleSingers: Boolean = false,
+    val promoteChasidish: Boolean = false,
+    val hideOldStuff: Boolean = false,
+)
+
+object ContentFilterState {
+    private val _state = kotlinx.coroutines.flow.MutableStateFlow(ContentFilterConfig())
+    val state: kotlinx.coroutines.flow.StateFlow<ContentFilterConfig> = _state
+
+    var current: ContentFilterConfig
+        get() = _state.value
+        internal set(value) {
+            _state.value = value
+        }
+}
