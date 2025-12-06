feefa292711d00eaf42aa10450bdc1b4fa0499e5
TripleU <tripleuworld@gmail.com>
2025-12-06 18:54:44 -0500

    Fix compile warnings for hilt and exhaustive when

--- app/src/main/kotlin/com/jtech/zemer/ui/screens/playlist/DownloadedContentScreen.kt
+++ app/src/main/kotlin/com/jtech/zemer/ui/screens/playlist/DownloadedContentScreen.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jtech.zemer.LocalPlayerAwareWindowInsets
 import com.jtech.zemer.R
